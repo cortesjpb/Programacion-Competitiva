0fb0b68d75bcb172e40d61c33a51c72e878dfa40
cortesjpb <48661799+cortesjpb@users.noreply.github.com>
2019-04-11 22:48:53 -0300

    update

--- Competitive Programming 3/05 Mathematics/BigDecimal.py
+++
@@ -1,4 +0,0 @@
-from decimal import *	#Import Decimal
-getcontext().prec=32	#Set precision
-ans=format(ans,'.32f')	#Print without Scientific Notation
-str(ans).rstrip('0')	#Delete trailing Zero
--- Competitive Programming 3/05 Mathematics/convertb.py
+++
@@ -1,4 +0,0 @@
-cs="0123456789ABCDEF"	#convierte un int o BigInt a cualquier base
-def convb(n,b):   
-   if n<b:return cs[n]
-   else:return convb(n//b,b)+cs[n%b]
